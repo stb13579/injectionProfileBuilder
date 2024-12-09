c3d361397648a44707f943905826294818659f1b
Shaun Brown <97027841+stb13579@users.noreply.github.com>
2024-12-09 18:53:27 +0000

    update front settings

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Important for correct asset paths
+  server: {
+    port: process.env.PORT || 3000
+  }
 })
