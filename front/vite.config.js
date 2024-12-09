e046d762b983adb8a857e75056a4edb81037377d
Shaun Brown <97027841+stb13579@users.noreply.github.com>
2024-12-09 18:58:56 +0000

    add host

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for correct asset paths
   server: {
+    host: '0.0.0.0', // This allows external access
+    port: process.env.PORT || 3000
+  },
+  preview: {
+    host: '0.0.0.0', // Important for production preview
     port: process.env.PORT || 3000
   }
 })
