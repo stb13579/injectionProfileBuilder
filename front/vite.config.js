081292aecb4702e79972f6dc609e101b57c7e57f
Shaun Brown <97027841+stb13579@users.noreply.github.com>
2024-12-12 19:49:34 +0000

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -29,11 +29,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 3000
+    host: '0.0.0.0',
+    port: "PORT"
   },
   preview: {
-    host: true,
-    port: 3000
+    host: '0.0.0.0',
+    port: "PORT"
   }
 })
