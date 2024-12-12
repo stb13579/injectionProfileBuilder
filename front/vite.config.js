a5b1e0fad0bc390464b961661b16cbe641b6ba3c
Shaun Brown <97027841+stb13579@users.noreply.github.com>
2024-12-12 19:20:15 +0000

    config for prod

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,24 +1,39 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'node:url'
-import path from 'node:path'
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { fileURLToPath } from 'node:url'
+// import path from 'node:path'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, __dirname, '')
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, __dirname, '')
   
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': env
-    },
-    server: {
-      port: env.PORT ? parseInt(env.PORT) : 3000
-    },
-    preview: {
-      port: env.PORT ? parseInt(env.PORT) : 3000
-    }
+//   return {
+//     plugins: [react()],
+//     define: {
+//       'process.env': env
+//     },
+//     server: {
+//       port: env.PORT ? parseInt(env.PORT) : 3000
+//     },
+//     preview: {
+//       port: env.PORT ? parseInt(env.PORT) : 3000
+//     }
+//   }
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: import.meta.env.PORT ? parseInt(import.meta.env.PORT) : 3000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: import.meta.env.PORT ? parseInt(import.meta.env.PORT) : 3000
   }
 })
