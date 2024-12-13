ef23780d37f6b4025e8ac1f129bb0fb340d693fa
Shaun Brown <97027841+stb13579@users.noreply.github.com>
2024-12-13 17:28:43 +0000

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,39 +1,24 @@
-// import { defineConfig, loadEnv } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import { fileURLToPath } from 'node:url'
-// import path from 'node:path'
-
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
-
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, __dirname, '')
-  
-//   return {
-//     plugins: [react()],
-//     define: {
-//       'process.env': env
-//     },
-//     server: {
-//       port: env.PORT ? parseInt(env.PORT) : 3000
-//     },
-//     preview: {
-//       port: env.PORT ? parseInt(env.PORT) : 3000
-//     }
-//   }
-// })
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: "PORT"
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: "PORT"
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname, '')
+  
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env
+    },
+    server: {
+      port: env.PORT ? parseInt(env.PORT) : 3000
+    },
+    preview: {
+      port: env.PORT ? parseInt(env.PORT) : 3000
+    }
   }
 })
