5c4c95f9cf38656f4ab0868c2d1714ebc291e4aa
Shaun Brown <97027841+stb13579@users.noreply.github.com>
2024-12-13 17:48:01 +0000

    config update

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,24 +1,17 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'node:url'
-import path from 'node:path'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, __dirname, '')
-  
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': env
-    },
-    server: {
-      port: env.PORT ? parseInt(env.PORT) : 3000
-    },
-    preview: {
-      port: env.PORT ? parseInt(env.PORT) : 3000
-    }
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000
   }
 })
