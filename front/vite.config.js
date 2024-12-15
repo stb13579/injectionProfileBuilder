2c3d489633df0ba0a07a3822c2d04cf73de13217
Shaun Brown <97027841+stb13579@users.noreply.github.com>
2024-12-15 15:11:13 +0000

    add Caddy

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: "../public",
+  root: "./src",
+  envDir: "../",
   build: {
-    outDir: 'dist'
+    outDir: "../dist",
+    emptyOutDir: true,
   },
   server: {
-    host: '0.0.0.0',
-    port: 3000
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 3000
+    host: "0.0.0.0"
   }
 })
